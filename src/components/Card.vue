c075a4706a50b02a189b826f78be2e5ff41e80bd
jiuyan9377 <2297006922@qq.com>
2023-06-04 14:31:21 +0800

    主体卡片设计

---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<script setup></script>
+<template>
+	<div class="card">
+		<div class="card-header">
+			<p class="time">06/01 18:58</p>
+			<p class="label">留言</p>
+		</div>
+		<div class="card-main">
+			<p>
+				新的项目，访问设计稿：www.huohuo90.com
+				入口进入。本人开发爱好者，欢迎交流。如有错误5122（或满）、801207724（或满）814700591，个人qq:1334785356,欢迎加入,加入请注明b站谢谢。觉得还可以，请多支持，谢谢。
+			</p>
+		</div>
+		<div class="card-bottom">
+			<div class="state">点赞</div>
+			<div class="name">张三</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+	width: 288px;
+	margin: 6px;
+	padding: 10px;
+	background-color: rgba($color: #fcafa2, $alpha: 0.4);
+	transition: all 0.3s;
+	.card-header {
+		display: flex;
+		justify-content: space-between;
+		p {
+			font-size: 16px;
+			color: var(--gray-2);
+		}
+	}
+	.card-main {
+		max-width: 260px;
+		padding: 20px 0;
+		p {
+			font-size: 14px;
+			line-height: 1.4;
+			height: 140px;
+			cursor: pointer;
+		}
+	}
+	.card-bottom {
+		display: flex;
+		justify-content: space-between;
+		.name {
+			font-weight: 600;
+			color: var(--gray-1);
+		}
+	}
+}
+</style>
--- src/views/mesgIndex.vue
+++ src/views/mesgIndex.vue
@@ -2,6 +2,8 @@
 import { getLists } from "../api/api";
 import Topbar from "../components/Topbar.vue";
 import Footbar from "../components/Footbar.vue";
+import Card from "@/components/Card.vue";
+import { ref } from "vue";
 const arr = {
 	type: 1,
 	message: "213213",
@@ -12,6 +14,11 @@ const arr = {
 	color: "red",
 	imgurl: "eeeee",
 };
+const selected = ref(-1);
+const list = ["留言", "目标", "理想", "爱情", "学业"];
+const toList = (index) => {
+	selected.value = index;
+};
 async function onClick() {
 	let res = await getLists(arr);
 	console.log(res);
@@ -20,13 +27,85 @@ async function onClick() {
 
 <template>
 	<Topbar />
+	<div class="content">
+		<div class="title">
+			<h1 class="name">留言墙</h1>
+			<p class="slogan">“不计划太多反而会勇敢冒险”</p>
+		</div>
+		<ul class="label">
+			<li
+				class="label-item"
+				:class="{ selected: selected == -1 }"
+				@click="toList(-1)">
+				全部
+			</li>
+			<li
+				class="label-item"
+				:class="{ selected: selected == index }"
+				v-for="index in list"
+				:key="index"
+				@click="toList(index)">
+				{{ index }}
+			</li>
+		</ul>
+		<div class="main">
+			<Card class="node-list" v-for="index in 12" :key="index"></Card>
+		</div>
+		.
+	</div>
 	<!-- <Footbar /> -->
 </template>
 
-<style lang="less" scoped>
-.title {
-	.top {
-		height: 100px;
+<style lang="scss" scoped>
+.content {
+	width: 1200px;
+	margin: 0 auto;
+	padding-top: 52px;
+	.title {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		.name {
+			font-size: 48px;
+			padding-top: 30px;
+			color: var(--gray-1);
+		}
+		.slogan {
+			padding: 10px 0;
+			color: var(--gray-3);
+		}
+	}
+	.label {
+		display: flex;
+		justify-content: center;
+		.label-item {
+			padding: 0 14px;
+			margin: 0 4px;
+			min-width: 16px;
+			color: var(--gray-2);
+			height: 28px;
+			line-height: 28px;
+			border-radius: 16px;
+			cursor: pointer;
+		}
+		.selected {
+			color: var(--prinary-color);
+			font-weight: 600;
+			border: 2px solid var(--prinary-color);
+		}
+	}
+	.main {
+		padding-top: 20px;
+		display: flex;
+		flex-wrap: wrap;
+		margin: auto;
+		.node-list {
+			height: 240px;
+		}
+
+		::v-deep(.card:hover) {
+			margin-top: 0;
+		}
 	}
 }
 </style>
